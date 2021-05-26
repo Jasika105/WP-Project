fae04f02c5ac1be2c530d8e6ff7b2d8bfd99ab72
Your Name <jasika105@gmail.com>
2021-05-27 01:13:46 +0600

    secound commit

---
+++ btn.js
@@ -0,0 +1,6 @@
+let btnOpen = document.querySelector('button');
+let input = document.querySelector('input');
+
+btnOpen.addEventListener('click', () =>{
+    window.open(input.value, '_blank');
+});
